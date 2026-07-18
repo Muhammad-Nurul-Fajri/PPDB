4fe9098a5f52adf1a5788a3f6e6eb7a515bda229
Muhammad Nurul Fajri <muhammadnurulfajri1602@gmail.com>
2026-07-18 10:41:36 +0700

    Add new controllers and update routes for landing, announcements, login, and registration
    - Created Landing_controller, Login_controller, Pengumuman_controller, and Registrasi_controller with basic structure.
    - Updated web.php to include routes for the new controllers.

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/Landing_controller', [App\Http\Controllers\Landing_controller::class, 'index']);
+Route::get('/Pengumuman_controller', [App\Http\Controllers\Pengumuman_controller::class, 'index']);
+Route::get('/Login_controller', [App\Http\Controllers\Login_controller::class, 'index']);
+Route::get('/Registrasi_controller', [App\Http\Controllers\Registrasi_controller::class, 'index']);
+Route::get('/');
+Route::get('/');
