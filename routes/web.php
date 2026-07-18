b954b145ee045284424c927e35c9b1aca9b27ac2
Muhammad Nurul Fajri <muhammadnurulfajri1602@gmail.com>
2026-07-18 10:42:52 +0700

    feat: add Dashboard_admin and Dashboard_siswa controllers with routing

--- routes/web.php
+++ routes/web.php
@@ -6,5 +6,5 @@ Route::get('/Landing_controller', [App\Http\Controllers\Landing_controller::clas
 Route::get('/Pengumuman_controller', [App\Http\Controllers\Pengumuman_controller::class, 'index']);
 Route::get('/Login_controller', [App\Http\Controllers\Login_controller::class, 'index']);
 Route::get('/Registrasi_controller', [App\Http\Controllers\Registrasi_controller::class, 'index']);
-Route::get('/');
-Route::get('/');
+Route::get('/Dashboard_admin_controller', [App\Http\Controllers\Dashboard_admin_controller::class, 'index']);
+Route::get('/Dashboard_siswa_controller', [App\Http\Controllers\Dashboard_siswa_controller::class, 'index']);
